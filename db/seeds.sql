0ee331a38a6b425d17025332072cd7518d8b4b90
DiegoCamilli <camillidiego@yahoo.com>
2023-05-23 13:39:53 -0700

    fixed issue with rendering posts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,34 +1,34 @@
-INSERT INTO chefs (first_name, last_name, email, passwords)
-VALUES ("John", "Smith", "john.smith@example.com", "P@ssword1"),
-        ("Emily", "Johnson", "emily.johnson@example.com", "P@ssword2"),
-        ("Maya", "Singh", "maya.singh@example.com", "P@ssword3"),
-        ("Sarah", "Davis", "sarah.davis@example.com", "P@ssword4"),
-        ("Elizabeth", "Taylor", "elizabeth.taylor@example.com", "P@ssword6"),
-        ("William", "Brown", "william.brown@example.com", "P@ssword5"),        
-        ("David", "Wilson", "david.wilson@example.com", "P@ssword7"),
-        ("Jennifer", "Anderson", "jennifer.anderson@example.com", "P@ssword8"),
-        ("Kwame", "Asante", "kwame.asante@example.com", "P@ssword9"),
-        ("Samantha", "Jackson", "samantha.jackson@example.com", "P@ssword10"),
-        ("Daniel", "Garcia", "daniel.garcia@example.com", "P@ssword11");
+-- INSERT INTO chefs (first_name, last_name, email, passwords)
+-- VALUES ("John", "Smith", "john.smith@example.com", "P@ssword1"),
+--         ("Emily", "Johnson", "emily.johnson@example.com", "P@ssword2"),
+--         ("Maya", "Singh", "maya.singh@example.com", "P@ssword3"),
+--         ("Sarah", "Davis", "sarah.davis@example.com", "P@ssword4"),
+--         ("Elizabeth", "Taylor", "elizabeth.taylor@example.com", "P@ssword6"),
+--         ("William", "Brown", "william.brown@example.com", "P@ssword5"),        
+--         ("David", "Wilson", "david.wilson@example.com", "P@ssword7"),
+--         ("Jennifer", "Anderson", "jennifer.anderson@example.com", "P@ssword8"),
+--         ("Kwame", "Asante", "kwame.asante@example.com", "P@ssword9"),
+--         ("Samantha", "Jackson", "samantha.jackson@example.com", "P@ssword10"),
+--         ("Daniel", "Garcia", "daniel.garcia@example.com", "P@ssword11");
         
-INSERT INTO kitchens (cuisines)
-VALUES (
-    "Italian cuisine"),
-    ("French cuisine"),
-    ("Chinese cuisine"),
-    ("Japanese cuisine"),
-    ("Mexican cuisine"),
-    ("Thai cuisine"),
-    ("Indian cuisine"),
-    ("Spanish cuisine"),
-    ("Greek cuisine"),
-    ("Mediterranean cuisine"),
-    ("American Food");
+-- INSERT INTO kitchens (cuisines)
+-- VALUES (
+--     "Italian cuisine"),
+--     ("French cuisine"),
+--     ("Chinese cuisine"),
+--     ("Japanese cuisine"),
+--     ("Mexican cuisine"),
+--     ("Thai cuisine"),
+--     ("Indian cuisine"),
+--     ("Spanish cuisine"),
+--     ("Greek cuisine"),
+--     ("Mediterranean cuisine"),
+--     ("American Food");
 
 
-INSERT INTO dishes (dish, recipes, descriptions, ingredients, dish_picture_id)
-VALUES (
+-- INSERT INTO dishes (dish, recipes, descriptions, ingredients, dish_picture_id)
+-- VALUES (
 
-)
+-- )
 
-INSERT INTO posts 
+-- INSERT INTO posts 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // const cloudinaryConfig = cloudinary.config({
 //   cloud_name: process.env.CLOUDNAME,
@@ -70,5 +70,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-app.listen(process.env.PORT || 3001)
